24c1a8a70c732ca6f3e3a6f6ccb73d2d49557ef6
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2023-04-11 09:20:33 +0300

    add models

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const logger = require('morgan')
 const cors = require('cors')
 
 const contactsRouter = require('./routes/api/contacts');
-console.log(contactsRouter)
+
 
 const app = express()
- 
+
 
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
@@ -22,7 +22,7 @@ app.use((req, res) => {
 })
 
 app.use((err, req, res, next) => {
-  const {status = 500, message = "Server error"}=err;
+  const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message })
 })
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,19 +1,52 @@
 const router = require("express").Router();
 
-router.get("./",(res,req)=>{
-    res.json("Get customers")
+const contacts = require("../../models/contacts")
+
+router.get("/", async (req, res) => {
+
+    const result = await contacts.listContacts();
+    res.json(result)
 });
-router.get("./:id",(res,req)=>{
-    res.json({message:"Customer"})
+router.get("/:id", async (req, res) => {
+
+    const { id } = req.params;
+
+    const result = await contacts.getContactById(id);
+
+
+    res.json(result)
+
 });
-router.post("./",(res,reg)=>{
-    res.send("create customers")
+router.post("/", async (req, res) => {
+
+
+    const { name, email, phone } = req.body;
+
+    const result = await contacts.addContact(name, email, phone)
+
+    res.status(201).json(result)
+
 });
-router.put("/:id",(res,req)=>{
-    res.send("update.customer")
+router.put("/:id", async (req, res) => {
+
+    const { id } = req.params;
+
+    const body = req.body
+
+
+    const result = await contacts.updateContact(id, body);
+
+
+    res.json(result)
+
 });
-router.delete("./:id",(res,req)=>{
-    res.send("remove customers")
+router.delete("/:id", async (req, res) => {
+
+    const { id } = req.params;
+    const result = await contacts.removeContact(id);
+
+    res.json({ message: "delete succes" })
+
 })
 
 
